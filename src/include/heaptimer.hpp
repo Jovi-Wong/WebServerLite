447f6ec9d3dcc801acb90ce877c546dd8f0f2688
Jovi Wong <1350386163@qq.com>
2022-08-29 10:33:50 -0500

    add class HeapTimer

---
+++ src/include/heaptimer.hpp
@@ -0,0 +1,62 @@
+#ifndef HEAP_TIMER
+#define HEAP_TIMER
+
+#include <queue>
+#include <unordered_map>
+#include <time.h>
+#include <algorithm>
+#include <arpa/inet.h> 
+#include <functional> 
+#include <assert.h> 
+#include <chrono>
+
+
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds MS;
+typedef Clock::time_point TimeStamp;
+
+struct TimerNode {
+    int id;
+    TimeStamp expires;
+    TimeoutCallBack cb;
+    bool operator<(const TimerNode& t) {
+        return expires < t.expires;
+    }
+};
+
+class HeapTimer {
+public:
+    HeapTimer() { heap_.reserve(64); }
+
+    ~HeapTimer() { clear(); }
+    
+    void adjust(int id, int newExpires);
+
+    void add(int id, int timeOut, const TimeoutCallBack& cb);
+
+    void doWork(int id);
+
+    void clear();
+
+    void tick();
+
+    void pop();
+
+    int GetNextTick();
+
+private:
+    void del_(size_t i);
+    
+    void siftup_(size_t i);
+
+    bool siftdown_(size_t index, size_t n);
+
+    void SwapNode_(size_t i, size_t j);
+
+    std::vector<TimerNode> heap_;
+
+    std::unordered_map<int, size_t> ref_;
+};
+
+#endif //HEAP_TIMER
