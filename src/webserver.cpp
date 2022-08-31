d6c6a24db8116732e5a31de694e015b883ca4b89
Jovi Wong <1350386163@qq.com>
2022-08-30 23:59:06 -0500

    Update webserver.cpp
    align code

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -10,30 +10,30 @@ WebServer::WebServer(
             port_(port), openLinger_(OptLinger), timeoutMS_(timeoutMS), isClose_(false),
             timer_(new HeapTimer()), threadpool_(new ThreadPool(threadNum)), epoller_(new Epoller())
     {
-    srcDir_ = getcwd(nullptr, 256);
-    assert(srcDir_);
-    strncat(srcDir_, "/resources/", 16);
-    HttpConn::userCount = 0;
-    HttpConn::srcDir = srcDir_;
-    SqlConnPool::Instance()->Init("localhost", sqlPort, sqlUser, sqlPwd, dbName, connPoolNum);
+        srcDir_ = getcwd(nullptr, 256); //
+        assert(srcDir_);
+        strncat(srcDir_, "/resources/", 16);
+        HttpConn::userCount = 0;
+        HttpConn::srcDir = srcDir_;
+        SqlConnPool::Instance()->Init("localhost", sqlPort, sqlUser, sqlPwd, dbName, connPoolNum);
 
-    InitEventMode_(trigMode);
-    if(!InitSocket_()) { isClose_ = true;}
+        InitEventMode_(trigMode);
+        if(!InitSocket_()) { isClose_ = true;}
 
-    if(openLog) {
-        Log::Instance()->init(logLevel, "./log", ".log", logQueSize);
-        if(isClose_) { LOG_ERROR("========== Server init error!=========="); }
-        else {
-            LOG_INFO("========== Server init ==========");
-            LOG_INFO("Port:%d, OpenLinger: %s", port_, OptLinger? "true":"false");
-            LOG_INFO("Listen Mode: %s, OpenConn Mode: %s",
-                            (listenEvent_ & EPOLLET ? "ET": "LT"),
-                            (connEvent_ & EPOLLET ? "ET": "LT"));
-            LOG_INFO("LogSys level: %d", logLevel);
-            LOG_INFO("srcDir: %s", HttpConn::srcDir);
-            LOG_INFO("SqlConnPool num: %d, ThreadPool num: %d", connPoolNum, threadNum);
+        if(openLog) {
+            Log::Instance()->init(logLevel, "./log", ".log", logQueSize);
+            if(isClose_) { LOG_ERROR("========== Server init error!=========="); }
+            else {
+                LOG_INFO("========== Server init ==========");
+                LOG_INFO("Port:%d, OpenLinger: %s", port_, OptLinger? "true":"false");
+                LOG_INFO("Listen Mode: %s, OpenConn Mode: %s",
+                                (listenEvent_ & EPOLLET ? "ET": "LT"),
+                                (connEvent_ & EPOLLET ? "ET": "LT"));
+                LOG_INFO("LogSys level: %d", logLevel);
+                LOG_INFO("srcDir: %s", HttpConn::srcDir);
+                LOG_INFO("SqlConnPool num: %d, ThreadPool num: %d", connPoolNum, threadNum);
+            }
         }
-    }
 }
 
 WebServer::~WebServer() {
