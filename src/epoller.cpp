49cb52d9c671d63e75f9b432a41f1f42abfc4674
Jovi Wong <1350386163@qq.com>
2022-09-01 11:50:04 -0500

    bracket style

--- src/epoller.cpp
+++ src/epoller.cpp
@@ -1,14 +1,17 @@
 #include "epoller.hpp"
 
-Epoller::Epoller(int maxEvent):epollFd_(epoll_create(512)), events_(maxEvent){
+Epoller::Epoller(int maxEvent):epollFd_(epoll_create(512)), events_(maxEvent)
+{
     assert(epollFd_ >= 0 && events_.size() > 0);
 }
 
-Epoller::~Epoller() {
+Epoller::~Epoller() 
+{
     close(epollFd_);
 }
 
-bool Epoller::AddFd(int fd, uint32_t events) {
+bool Epoller::AddFd(int fd, uint32_t events)
+{
     if(fd < 0) return false;
     epoll_event ev = {0};
     ev.data.fd = fd;
@@ -16,7 +19,8 @@ bool Epoller::AddFd(int fd, uint32_t events) {
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &ev);
 }
 
-bool Epoller::ModFd(int fd, uint32_t events) {
+bool Epoller::ModFd(int fd, uint32_t events) 
+{
     if(fd < 0) return false;
     epoll_event ev = {0};
     ev.data.fd = fd;
@@ -24,22 +28,26 @@ bool Epoller::ModFd(int fd, uint32_t events) {
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &ev);
 }
 
-bool Epoller::DelFd(int fd) {
+bool Epoller::DelFd(int fd)
+{
     if(fd < 0) return false;
     epoll_event ev = {0};
     return 0 == epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &ev);
 }
 
-int Epoller::Wait(int timeoutMs) {
+int Epoller::Wait(int timeoutMs)
+{
     return epoll_wait(epollFd_, &events_[0], static_cast<int>(events_.size()), timeoutMs);
 }
 
-int Epoller::GetEventFd(size_t i) const {
+int Epoller::GetEventFd(size_t i) const
+{
     assert(i < events_.size() && i >= 0);
     return events_[i].data.fd;
 }
 
-uint32_t Epoller::GetEvents(size_t i) const {
+uint32_t Epoller::GetEvents(size_t i) const
+{
     assert(i < events_.size() && i >= 0);
     return events_[i].events;
 }
